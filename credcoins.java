44620ae96609766c36fae0c84d52a8148179ee0d
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2025-01-03 21:39:22 +0530

    Add files via upload

---
+++ credcoins.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner S=new Scanner(System.in);
+	    int t=S.nextInt();
+	    for(int i=0;i<t;i++)
+	    { 
+	        int x=S.nextInt();
+	        int y=S.nextInt();
+	        int z=(x*y)/100;
+	        System.out.println(z);
+
+	}
+}
+}
